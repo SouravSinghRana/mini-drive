286c2a3d73176f37e3d9387b907c5ec7ef5b24ca
SouravSinghRana <41117593+SouravSinghRana@users.noreply.github.com>
2021-02-27 19:01:04 +0530

    added UI for SignUp component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import SignUp from './SignUp';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
-    <div>
-      <p>hello</p>
-    </div>
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight:"100vh" }}
+    >
+      <div className="w-100" style={{maxWidth: "400px"}} >
+        <SignUp />
+      </div>
+    </Container>
   );
 }
 
